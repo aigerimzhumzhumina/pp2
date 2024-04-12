0654dfe81a779872937825d0749fa669212ab3c5
aigerimzhumzhumina <a_zhumzhumina@kbtu.kz>
2024-04-12 12:13:55 +0500

    Lab 8 final version

--- Lab8/paint/game.py
+++ Lab8/paint/game.py
@@ -1,24 +1,115 @@
-import sys, pygame 
-from pygame.locals import *
-pygame.init() 
-screen = pygame.display.set_mode((1000,600))
-screen.fill((255, 255, 255))
-brush = pygame.image.load(r"Lab8\paint\brush.png")
-brush = pygame.transform.scale(brush,(128,128))
-pygame.display.update()
-clock = pygame.time.Clock()
-z = 0
+import pygame 
 
-while 1:
-    clock.tick(60)
-    x,y = pygame.mouse.get_pos()
+pygame.init() 
+
+
+WIDTH = 800 
+HEIGHT = 600
+
+colorRED = (255, 0, 0)
+colorBLUE = (0, 0, 255)
+colorWHITE = (255, 255, 255)
+colorBLACK = (0, 0, 0)
+colorGREEN = (0, 255, 0)
+colorYELLOW = (255, 255, 0)
+
+
+colordict = {
+    "red":colorRED,
+    "green":colorGREEN,
+    "blue":colorBLUE,
+    "yellow":colorYELLOW
+}
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+base_layer = pygame.Surface((WIDTH, HEIGHT))
+base_layer.fill(colorWHITE)
+screen.fill(colorWHITE)
+
+
+LMBpressed = False
+THICKNESS = 5
+
+currX = 0
+currY = 0
+
+prevX = 0
+prevY = 0
+
+mode = "rectangle"
+color_mode = "red"
+
+
+def calculate_rect(x1, y1, x2, y2):
+    return pygame.Rect(min(x1, x2), min(y1, y2), abs(x1 - x2), abs(y1 - y2))
+
+
+done = False
+
+while not done:
+
     for event in pygame.event.get():
+        if LMBpressed:
+            screen.blit(base_layer, (0, 0))
         if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == MOUSEBUTTONDOWN:
-            z = 1 
-        elif event.type == MOUSEBUTTONUP:
-            z = 0
-        if z == 1:
-            screen.blit(brush, (x-64,y-64))
-            pygame.display.update()
+            done = True
+
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            LMBpressed = True
+            prevX = event.pos[0]
+            prevY = event.pos[1]
+           
+        if event.type == pygame.MOUSEMOTION:
+            if LMBpressed:
+                currX = event.pos[0]
+                currY = event.pos[1]
+                
+                if mode == "rectangle":
+                    pygame.draw.rect(screen, colordict[color_mode], calculate_rect(prevX, prevY, currX, currY), THICKNESS)
+                if mode == "circle":
+                    pygame.draw.circle(screen, colordict[color_mode], (prevX,prevY),abs(currX-prevX), THICKNESS)
+                if mode == "eraser":
+                    pygame.draw.circle(screen, colorWHITE, (currX,currY), 20)
+                    base_layer.blit(screen, (0,0)) 
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            LMBpressed = False
+            currX = event.pos[0]
+            currY = event.pos[1]
+
+            if mode == "rectangle":
+                pygame.draw.rect(screen, colordict[color_mode], calculate_rect(prevX, prevY, currX, currY), THICKNESS)
+                base_layer.blit(screen, (0, 0))
+            if mode == "circle":
+                pygame.draw.circle(screen, colordict[color_mode], (prevX,prevY),abs(currX-prevX), THICKNESS)
+                base_layer.blit(screen, (0,0))
+            
+
+        if event.type == pygame.KEYDOWN: 
+            
+            
+            if event.key == pygame.K_SPACE:
+                mode = "circle"
+                print("drawing mode switched to circle")
+            if event.key == pygame.K_ESCAPE:
+                mode = "rectangle"
+                print("drawing mode switched to rectangle")
+            if event.key == pygame.K_e:
+                mode = "eraser"
+                print("eraser was chosen")
+
+            elif event.key == pygame.K_r:
+                color_mode = "red"
+                print("now the color is red")
+            elif event.key == pygame.K_b:
+                color_mode = "blue"
+                print("now the color is blue")
+            elif event.key == pygame.K_g:
+                color_mode = "green"
+                print("now the color is green")
+            elif event.key == pygame.K_y:
+                color_mode = "yellow"
+                print("now the color is yellow")
+        
+
+    pygame.display.flip()
--- Lab8/snake/snake.py
+++ Lab8/snake/snake.py
@@ -1,81 +1,147 @@
-import pygame
-import random
-from game_object import GameObject 
-from worm import Worm
-from food import Food
-from wall import Wall
+import pygame   
+from color_palette import *  
+import time   
+import random   
+pygame.init()   
 
-def create_background(screen, width, height):
-        colors = [(255, 255, 255), (212, 212, 212)]
-        tile_width = 20
-        y = 0
-        while y < height:
-                x = 0
-                while x < width:
-                        row = y // tile_width
-                        col = x // tile_width
-                        pygame.draw.rect(screen, colors[(row + col) % 2],pygame.Rect(x, y, tile_width, tile_width))
-                        x += tile_width
-                y += tile_width
+ 
+WIDTH = 600
+HEIGHT = 600
+CELL = 30
 
-done = False
+ 
+level = 1
+score = 0
+snake_speed = 5
 
+ 
+def draw_grid():
+    for i in range(HEIGHT // 2):
+        for j in range(WIDTH // 2):
+            pygame.draw.rect(screen, colorGRAY, (i * CELL, j * CELL, CELL, CELL), 1)
 
-pygame.init()
-screen = pygame.display.set_mode((400, 300))
+ 
+def draw_grid_chess():
+    colors = [colorWHITE, colorGRAY]
+
+    for i in range(HEIGHT // 2):
+        for j in range(WIDTH // 2):
+            pygame.draw.rect(screen, colors[(i + j) % 2], (i * CELL, j * CELL, CELL, CELL))
+
+ 
+screen = pygame.display.set_mode((HEIGHT, WIDTH))
+
+ 
+class Point:
+    def __init__(self, x, y):   
+        self.x = x
+        self.y = y
+
+    def __str__(self):   
+        return f"{self.x}, {self.y}"
+
+ 
+class Snake:
+    def __init__(self):  
+        self.body = [Point(10, 11), Point(10, 12), Point(10, 13)]  
+        self.dx = 1  
+        self.dy = 0   
+
+    def move(self):  
+        for i in range(len(self.body) - 1, 0, -1):  
+            self.body[i].x = self.body[i - 1].x   
+            self.body[i].y = self.body[i - 1].y
+
+        self.body[0].x += self.dx   
+        self.body[0].y += self.dy
+
+         
+        if self.body[0].x == -1 or self.body[0].x == 20 or self.body[0].y == -1 or self.body[0].y == 20:
+            return False
+
+    def draw(self):   
+        head = self.body[0]   
+        pygame.draw.rect(screen, colorGREEN, (head.x * CELL, head.y * CELL, CELL, CELL))  
+        for segment in self.body[1:]:   
+            pygame.draw.rect(screen, colorYELLOW, (segment.x * CELL, segment.y * CELL, CELL, CELL))  
+
+    def check_collision(self, food):   
+        head = self.body[0]   
+        if head.x == food.pos.x and head.y == food.pos.y:   
+            print("Got food!")  
+            self.body.append(Point(head.x, head.y))   
+            pygame.display.update()  
+            food.change_pos()  
+ 
+class Food:
+    def __init__(self):   
+        self.pos = Point(random.randrange(0, 19), random.randrange(0, 19))   
+
+    def generate_new_position(self):   
+        new_pos = Point(random.randrange(0, 19), random.randrange(0, 19))   
+        while new_pos in snake.body:   
+            new_pos = Point(random.randrange(0, 19), random.randrange(0, 19))
+        return new_pos   
+    def change_pos(self):   
+        global score   
+        self.pos = self.generate_new_position()   
+        score += 1  
+
+    def draw(self):   
+        pygame.draw.rect(screen, colorRED, (self.pos.x * CELL, self.pos.y * CELL, CELL, CELL))  
+ 
 clock = pygame.time.Clock()
 
-# Score and level
-speed = 5
-SCORE = 0
-LEVEL = 1
+ 
+food = Food()   
+snake = Snake()   
 
-worm = Worm(20)
-food = Food(20)
-wall = Wall(20)
+ 
+font = pygame.font.SysFont("Verdana", 60)
+game__over = font.render("Game Over", True, colorBLACK)
 
-while not done:
-        # Event filtering
-        filtered_events = []
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                    done = True
-            else:
-                filtered_events.append(event)
+ 
+done = False
+while not done:   
+    for event in pygame.event.get():  
+        if event.type == pygame.QUIT:   
+            done = True   
+        if event.type == pygame.KEYDOWN:  
+            if event.key == pygame.K_RIGHT and snake.dx != -1:   
+                snake.dx = 1   
+                snake.dy = 0
+            elif event.key == pygame.K_LEFT and snake.dx != 1:  
+                snake.dx = -1  
+                snake.dy = 0
+            elif event.key == pygame.K_DOWN and snake.dy != -1:   
+                snake.dx = 0   
+                snake.dy = 1
+            elif event.key == pygame.K_UP and snake.dy != 1:   
+                snake.dx = 0   
+                snake.dy = -1
+    
+    draw_grid_chess()  
 
-        worm.process_input(filtered_events)
-        worm.move()
+    font = pygame.font.SysFont(None, 25)   
+    text = font.render("Score: " + str(score) + "   Level: " + str(level), True, colorBLACK)  
+    screen.blit(text, (10, 10))   
 
-        food.time_out()
-
-        # check for wall collision
-        pos = wall.can_hit(worm.points[0])
-        if pos != None or worm.points[0].X < 0 or worm.points[0].X >= 400 or worm.points[0].Y < 0 or worm.points[0].Y >= 300:
-              done = True
-              break
-
-
-        pos = food.can_eat(worm.points[0])
-        if(pos != None):
-            SCORE += food.weight
-            worm.increase(pos)
-            for i in range(food.weight - 1):
-                pos.X += 1
-                worm.increase(pos)
+    if snake.move() == False:   
+        done = True  
+        screen.fill(colorRED)   
+        screen.blit(game__over, (125, 225))  
         
-            food.change_pos()
-            
-            if len(worm.points) > 5:
-                wall.next_level()
-                worm.next_level()
-                speed += 5
-                LEVEL = (LEVEL + 1) % 2
+        pygame.display.update()   
+        time.sleep(2)  
 
-        create_background(screen, 400, 300)
-        
-        food.draw(screen)
-        wall.draw(screen)
-        worm.draw(screen)
-        
-        pygame.display.flip()
-        clock.tick(speed)
+    snake.check_collision(food)   
+
+    if score > level * 3:   
+        level += 1   
+        snake_speed += 3   
+
+    snake.draw()  
+    food.draw()  
+
+    pygame.display.flip()   
+    clock.tick(snake_speed)   
